8d30ba05cb77b4dfa56643ae923d456d640eaefb
Victor 72 <coronelvictor651@gmail.com>
2025-07-13 15:49:41 -0300

    Globo_funcional

---
+++ caidaextremas_version_2.1/escenas/enemies/globo/globo.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+@onready var colision_derecha = $Colision_Derecha
+@onready var colision_izquierda = $Colision_Izquierda
+const SPEED = 100.0
+var gravity : int = 0
+var direccion : int = 1
+
+func _ready():
+	$AnimatedSprite2D.play()
+
+func _physics_process(delta):
+	movimiento_normal()
+	move_and_slide()
+
+func movimiento_normal():
+	if direccion == 1:
+		velocity.x = +SPEED
+		if colision_derecha.is_colliding():
+			direccion = -1
+	elif direccion == -1:
+		velocity.x = -SPEED 
+		if colision_izquierda.is_colliding():
+			direccion = 1
+
+func _on_derecha_body_entered(body):
+	if body.is_in_group("player"):
+		direccion = 1
+
+func _on_izquierda_body_entered(body):
+	if body.is_in_group("player"):
+		direccion = -1
+
+#func _on_derecha_body_exited(body):
+	#if body.is_in_group("player"):
+		#direccion = -1
+#
+#func _on_izquierda_body_exited(body):
+	#if body.is_in_group("player"):
+		#direccion = 1
+
+func _on_centro_body_entered(body):
+	if body.is_in_group("player"):
+		direccion = 0
