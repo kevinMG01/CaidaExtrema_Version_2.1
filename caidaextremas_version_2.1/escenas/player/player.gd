2d1417ae7b9027200a5dc25726a5333aed9e2995
kevinMG01 <montanokevin46@gmail.com>
2025-07-19 15:09:56 -0300

    habilidad
    función  para usar  "habilidades"

--- caidaextremas_version_2.1/escenas/player/player.gd
+++ caidaextremas_version_2.1/escenas/player/player.gd
@@ -1,54 +1,65 @@
 extends CharacterBody2D
- 
-var SPEED :float = 500.0         
-var JUMP_VELOCITY : float = -400.0
 
 var state: String
-
-var paracaidas_activado = true # se ejecuta cuando esta en el estado_aire y determina si tiene el paracaidas o se la quito
 
 const GRAVEDAD_NORMAL : float = 700.0
 const GRAVEDAD_PARACAIDAS : float = 300.0
 
+@export var habilidad_activa: String = "dash" # dash, super_salto,etc.
+
+var  is_dashing :bool = false
+var  is_super_salto :bool = false
+
+var atributos: Dictionary = {}
+var atributos_extras: Dictionary = {} # no esta programado, es para el futuro
 
 func _ready() -> void:
-	set_state("quitar_paracaidas")
+	for key in atributos_Base().keys():
+		atributos[key] = atributos_Base(key)
+		print(key)
 
+	#atributos["speed"] = 0
+
+	print(atributos)
+	atributos["speed"] = atributos_Base("speed")#para reemplasar un atributo
+	set_state("quitar_paracaidas")
+	print(atributos)
 
 func _physics_process(delta: float) -> void:
 	aplicar_gravedad(delta)
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction :int = Input.get_axis("ui_left", "ui_right")
 	move(direction)
 	update_state(direction)
 	move_and_slide()
 
-
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = atributos["jump"]
+		print(atributos)
 	elif Input.is_action_just_pressed("ui_up") and not is_on_floor():
-		if paracaidas_activado:
+		if atributos["paracaidas_activado"]:
 			set_state("quitar_paracaidas")
 		else:
 			set_state("abrir_paracaidas")
+	if Input.is_action_just_pressed("ui_down"):
+		ejecutar_habilidad(habilidad_activa)
 
-
-func move(direction):
+func move(direction): # controlamos el movimineto
 	if direction:
-		velocity.x = direction * SPEED 
+		velocity.x = direction * atributos["speed"]
 		$AnimatedSprite2D.flip_h = direction < 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, atributos["speed"])
 
-func aplicar_gravedad(delta):
+func aplicar_gravedad(delta):# aplicamos gravedad
 	if not is_on_floor():
-		if paracaidas_activado:
+		if atributos["paracaidas_activado"]:
 			velocity.y += GRAVEDAD_PARACAIDAS * delta
-			velocity.y = min(velocity.y, 300) # limita velocidad máxima de caída con paracaídas
+			velocity.y = min(velocity.y, atributos["estado_aire_MAX_VEL_CAIDA"]) # limita velocidad máxima de caída con paracaídas
 		else:
 			velocity.y += GRAVEDAD_NORMAL * delta
 
-func update_state(direction):
+func update_state(direction): # controla las animaciones 
 	if state in ["abrir_paracaidas"]:
 		return
 
@@ -68,7 +79,7 @@ func update_state(direction):
 		else:
 			set_state("run")
 	elif state in estados_paracaidas:
-		if paracaidas_activado:
+		if atributos["paracaidas_activado"]:
 			set_state("paracaidas_equipada")
 		else:
 			set_state("quitar_paracaidas")
@@ -76,7 +87,7 @@ func update_state(direction):
 	if is_on_floor() and state not in ["run", "jump"]:
 		set_state("idle")
 
-func set_state(new_state):
+func set_state(new_state): #ejecuta las animaciones
 	if state == new_state: # no permite un estado repetido.
 		return
 
@@ -86,7 +97,7 @@ func set_state(new_state):
 
 	match state:
 		"idle":
-			paracaidas_activado = false
+			atributos["paracaidas_activado"] = true
 			$AnimatedSprite2D.play("Idle")
 			print(state)
 		"run":
@@ -102,11 +113,11 @@ func set_state(new_state):
 			$AnimatedSprite2D.play("abrir_paracaidas")
 			print(state)
 		"paracaidas_equipada":
-			paracaidas_activado = true
+			atributos["paracaidas_activado"] = true
 			$AnimatedSprite2D.play("paracaidas_equipada")
 			print(state)
 		"quitar_paracaidas":
-			paracaidas_activado = false
+			atributos["paracaidas_activado"] = false
 			$AnimatedSprite2D.play("quitar_paracaidas")
 			print(state)
 
@@ -114,8 +125,7 @@ func _on_animated_sprite_2d_animation_finished() -> void:
 	if state == "abrir_paracaidas":
 		set_state("paracaidas_equipada")
 
-
-func collisionshape():
+func collisionshape():# controla que colisionshape2d deveria de estar activo y oculta la otras
  #Oculta todos los collision shapes
 	var collision_nodes = [$CollisionShape2D, $fall, $sin_paracaidas]
 	for node in collision_nodes:
@@ -129,3 +139,54 @@ func collisionshape():
 			$fall.visible = true
 		"quitar_paracaidas":
 			$sin_paracaidas.visible = true
+
+func ejecutar_habilidad(nombre: String):
+	if is_dashing and nombre == "dash":
+		return
+	if is_super_salto and nombre == "super_salto":
+		return
+
+	match nombre:
+		"dash":
+			is_dashing = true
+			atributos["speed"] = 2000.0
+			crear_timer(0.08, func():
+				atributos["speed"] = atributos_Base("speed")
+				is_dashing = false
+			)
+
+		"super_salto":
+			is_super_salto = true
+			atributos["jump"] = -900.0
+			crear_timer(4.0, func():
+				atributos["jump"] = atributos_Base("jump")
+				is_super_salto = false
+				
+			)
+
+func crear_timer(tiempo: float, callback: Callable):
+	var timer :Timer = Timer.new()
+	timer.one_shot = true
+	timer.wait_time = tiempo
+	add_child(timer)
+	timer.connect("timeout", callback)
+	
+	timer.connect("timeout", func():
+		callback.call()
+		timer.queue_free()
+	)
+	
+	timer.start()
+
+func atributos_Base(key :String= "") -> Variant:
+	var base: Dictionary = {
+		"speed": 500.0,
+		"jump": -400.0,
+		"paracaidas_activado": true,## se ejecuta cuando esta en el estado_aire y determina si tiene el paracaidas o se la quito
+		
+		"estado_aire_MAX_VEL_CAIDA": 300,
+		
+	}
+	if key != "":
+		return base.get(key, null)
+	return base
