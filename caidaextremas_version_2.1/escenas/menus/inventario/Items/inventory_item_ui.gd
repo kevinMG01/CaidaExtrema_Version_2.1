18b40d6f17d3252b58540c00dd84bf963d0b6608
kevinMG01 <montanokevin46@gmail.com>
2025-07-27 19:14:20 -0300

    creación de un contenedor para los ítems

--- caidaextremas_version_2.1/escenas/menus/inventario/Items/inventory_item_ui.gd
+++ caidaextremas_version_2.1/escenas/menus/inventario/Items/inventory_item_ui.gd
@@ -1,19 +1,16 @@
-extends Panel
+extends TextureRect
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
+
+
 
 func initialize(texture_rec, valor):
-	
-	$TextureRect.texture = texture_rec
+	texture = texture_rec
 	$Label.text = str(valor)
-	
+
 func set_quantity(valor):
 	$Label.text = str(valor)
--- caidaextremas_version_2.1/escenas/menus/inventario/inventario.gd
+++ caidaextremas_version_2.1/escenas/menus/inventario/inventario.gd
@@ -1,7 +1,7 @@
 extends Control
 
 
-@onready var item_list: ItemList = $VBoxContainer/HBoxContainer/ItemList
+@onready var grid_container: GridContainer = $VBoxContainer/HBoxContainer/GridContainer
 const INVENTORY_ITEM_UI = preload("res://escenas/menus/inventario/Items/inventory_item_ui.tscn")
 @onready var item_seleccionado: GridContainer = $HBoxContainer/VBoxContainer/Item_seleccionado
 
@@ -13,26 +13,18 @@ var texturas = [
 
 func _ready():
 	randomize()
-	var resultado = abrirCaja()
-	if resultado != "cajaVacia":
-		if resultado == "congelar":
-			add_item(resultado, texturas[0], 1)
-		elif resultado == "intangible":
-			add_item(resultado, texturas[1], 1)
-	else:
-		print("¡Caja vacía!")
 
 
 func add_item(type, texture: CompressedTexture2D, quantity: int):
 	var node_name = "inventory_item" + str(type)
-	if not item_list.has_node(node_name):
-		var inventory_item = INVENTORY_ITEM_UI.instantiate()
-		inventory_item.name = node_name
-		inventory_item.initialize(texture, quantity)
-		item_seleccionado.add_child(inventory_item) 
-	else:
-		var existing_node = item_seleccionado.get_node(node_name)
-		existing_node.set_quantity(quantity)
+	#if not item_list.has_node(node_name):
+	var inventory_item = INVENTORY_ITEM_UI.instantiate()
+	inventory_item.name = node_name
+	inventory_item.initialize(texture, quantity)
+	grid_container.add_child(inventory_item) 
+	#else:
+		#var existing_node = item_seleccionado.get_node(node_name)
+		#existing_node.set_quantity(quantity)
 
 
 var probabilidades = {
@@ -43,7 +35,7 @@ var probabilidades = {
 
 func pawerUpEnUso(newslot):
 	var duplicated_slot = newslot.duplicate()
-	item_list.add_child(duplicated_slot)
+	grid_container.add_child(duplicated_slot)
 	duplicated_slot.custom_minimum_size = Vector2(70, 70)
 
 func abrirCaja():
@@ -54,6 +46,16 @@ func abrirCaja():
 		probabilidad_acumulada += probabilidades[objeto]
 		if valor_aleatorio <= probabilidad_acumulada:
 			return objeto
-			
 
 	return "cajaVacia"
+
+
+func _on_caja_button_down() -> void:
+	var resultado = abrirCaja()
+	if resultado != "cajaVacia":
+		if resultado == "congelar":
+			add_item(resultado, texturas[0], 1)
+		elif resultado == "intangible":
+			add_item(resultado, texturas[1], 1)
+	else:
+		print("¡Caja vacía!")
