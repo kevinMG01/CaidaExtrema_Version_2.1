5c354d1e0a0fa3acce542cb5a71f2c966077f8b3
Victor 72 <coronelvictor651@gmail.com>
2025-08-19 00:21:00 -0300

    Niveles del 5 al 15

---
+++ caidaextremas_version_2.1/escenas/menus/menu_de_niveles/zona_3_ave_en_picada/nivel_11.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$player.nivel_camara = 1
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_meta_body_entered(body):
+	if body.is_in_group("player"):
+		if GlobalVar.nivel_desbloqueado == 11:
+			GlobalVar.nivel_desbloqueado += 1 
+		get_tree().change_scene_to_file("res://escenas/menus/menu_de_niveles/Menu_de_zonas.tscn")
