1bd2a6a5bbb0195e4bdd92d4ab1b2ea76ab33c5e
kevinMG01 <montanokevin46@gmail.com>
2025-08-27 11:38:54 -0300

    arreglos de muerte

--- caidaextremas_version_2.1/escenas/menus/menu_dead/menu_dead.gd
+++ caidaextremas_version_2.1/escenas/menus/menu_dead/menu_dead.gd
@@ -1,10 +1,17 @@
 extends Control
 
-@export var nivel : Node2D
-var escena : Array = []
+@export var nivel : int
+var escena : Array = [
+	"",
+	"res://escenas/menus/menu_de_niveles/zona_1_globos/nivel_1/nivel_1.tscn",
+	"res://escenas/menus/menu_de_niveles/zona_1_globos/nivel_2/nivel_2.tscn",
+	"res://escenas/menus/menu_de_niveles/zona_1_globos/nivel_3/nivel_3.tscn",
+	"res://escenas/menus/menu_de_niveles/zona_1_globos/nivel_4/nivel_4.tscn",
+	"res://escenas/menus/menu_de_niveles/zona_1_globos/nivel_5/nivel_5.tscn",
+]
 
 func _on_retry_pressed() -> void:
-	get_tree().change_scene_to_file(escena)
+	get_tree().change_scene_to_file(escena[nivel])
 	pass # Replace with function body.
 
 
